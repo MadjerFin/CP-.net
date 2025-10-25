e686a3b1c8291ee7762791b6544718996ca5c233
Bruno Cantacini <brunocantacini.ti@gmail.com>
2025-10-24 22:42:35 -0300

    teste

--- Cp1/Entities/Consulta.cs
+++ Cp1/Entities/Consulta.cs
@@ -7,6 +7,8 @@ public class Consulta
     public int Id {get; set;}
     public DateTime Data {get; set;}
     public TimestampAttribute Hora {get; set;}
+
+    // Enum para o status da consulta
     public char Status {get; set;} // A = Agendada, C = Cancelada, R = Realizada
     public string Observacoes  {get; set;} 
     
